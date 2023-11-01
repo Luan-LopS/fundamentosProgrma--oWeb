cbffd35db0bfcc9dbb3beebd8f045fdd8c9ebcb6
Luan <luam10@hotmail.com.br>
2023-11-01 18:56:47 -0300

    projeto finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 // criando opções de produtos com js
-
-
-
 function mostraProdutos(){
+    let produtos = document.getElementsByClassName('produtos')[0]
     let fantasia = document.getElementById('fantasia')
     let decoracao = document.getElementById('decoracao')
     let comida = document.getElementById('comida')
@@ -14,24 +12,59 @@ function mostraProdutos(){
     fantasia.addEventListener('click', ()=>{
         if (ordem.style.display === 'flex') {
             ordem.style.display = 'none'
+            produtos.style.height = '200px'
+            fantasia.style.background =''
+
+            
         } else {
             ordem.style.display = 'flex'
+            ordem1.style.display = 'none'
+            ordem2.style.display = 'none'
+            produtos.style.height = '400px'
+            fantasia.style.background ='red'
+            ordem.style.background = 'red'
+            decoracao.style.background =''
+            comida.style.background = ''
+
         }
     })
     
     decoracao.addEventListener('click', ()=>{
         if (ordem1.style.display === 'flex') {
             ordem1.style.display = 'none'
+            produtos.style.height = '200px'
+            decoracao.style.background =''
+
+
         } else {
             ordem1.style.display = 'flex'
+            ordem.style.display = 'none'
+            ordem2.style.display = 'none'
+            produtos.style.height = '400px'
+            decoracao.style.background ='blue'
+            ordem1.style.background = 'blue'
+            fantasia.style.background =''
+            comida.style.background = ''
+
         }
     })
     
     comida.addEventListener('click', ()=>{
         if (ordem2.style.display === 'flex') {
             ordem2.style.display = 'none'
+            produtos.style.height = '200px'
+            comida.style.background = ''
+
         } else {
             ordem2.style.display = 'flex'
+            ordem1.style.display = 'none'
+            ordem.style.display = 'none'
+            produtos.style.height = '400px'
+            ordem2.style.background = 'yellow'
+            comida.style.background = 'yellow'
+            fantasia.style.background =''
+            decoracao.style.background =''
+
         }
     })
     
