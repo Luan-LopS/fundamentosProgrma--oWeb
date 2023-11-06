3009cd930163f6f0fe40098e2417c2bd1d5b6cd6
Luan <luam10@hotmail.com.br>
2023-11-06 01:01:36 -0300

    teste

--- scripts/dados.js
+++ scripts/dados.js
@@ -22,22 +22,3 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'formAction.html';
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const nomeRecebido = document.getElementById('nome-recebido');
-    const emailRecebido = document.getElementById('email-recebido');
-    const foneRecebido = document.getElementById('fone-recebido');
-
-    // Recupera os dados do localStorage
-    const nome = localStorage.getItem('nome');
-    const email = localStorage.getItem('email');
-    const fone = localStorage.getItem('fone');
-
-    // Exibe os dados na página
-    if (nome) {
-        nomeRecebido.innerHTML += nome;
-        emailRecebido.innerHTML += email;
-        foneRecebido.innerHTML += fone;
-    }
-
-});
